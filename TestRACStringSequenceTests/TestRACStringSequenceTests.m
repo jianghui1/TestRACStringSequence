07dc139993dbb9fe9c71b12e8888435aa75eb6a2
jiangzenghui005@163.com <jiangzenghui005@163.com>
2018-08-17 18:31:52 +0800

    测试用例提交

--- TestRACStringSequenceTests/TestRACStringSequenceTests.m
+++ TestRACStringSequenceTests/TestRACStringSequenceTests.m
@@ -8,32 +8,61 @@
 
 #import <XCTest/XCTest.h>
 
+#import <ReactiveCocoa.h>
+#import <RACStringSequence.h>
+
 @interface TestRACStringSequenceTests : XCTestCase
 
 @end
 
 @implementation TestRACStringSequenceTests
 
-- (void)setUp {
-    [super setUp];
-    // Put setup code here. This method is called before the invocation of each test method in the class.
+- (void)test_sequenceWithString
+{
+    RACSequence *sequence = [RACStringSequence sequenceWithString:@"abc" offset:0];
+    NSLog(@"sequenceWithString -- %@", sequence);
+    
+    // 打印日志：
+    /*
+     2018-08-17 17:27:52.782259+0800 TestRACStringSequence[51424:18355473] sequenceWithString -- <RACStringSequence: 0x6040004317e0>{ name = , string = abc }
+     */
 }
 
-- (void)tearDown {
-    // Put teardown code here. This method is called after the invocation of each test method in the class.
-    [super tearDown];
+- (void)test_head
+{
+    RACSequence *sequence = [RACStringSequence sequenceWithString:@"abc" offset:0];
+    NSLog(@"head -- %@", sequence.head);
+    
+    // 打印日志：
+    /*
+     2018-08-17 17:30:05.796562+0800 TestRACStringSequence[51512:18362582] head -- a
+     */
 }
 
-- (void)testExample {
-    // This is an example of a functional test case.
-    // Use XCTAssert and related functions to verify your tests produce the correct results.
+- (void)test_tail
+{
+    RACSequence *sequence = [RACStringSequence sequenceWithString:@"abc" offset:0];
+    NSLog(@"tail -- %@", sequence.tail);
+    
+    // 打印日志：
+    /*
+     2018-08-17 17:31:20.424395+0800 TestRACStringSequence[51571:18366588] tail -- <RACStringSequence: 0x604000231cc0>{ name = , string = bc }
+     */
 }
 
-- (void)testPerformanceExample {
-    // This is an example of a performance test case.
-    [self measureBlock:^{
-        // Put the code you want to measure the time of here.
-    }];
+- (void)test_array
+{
+    RACSequence *sequence = [RACStringSequence sequenceWithString:@"abc" offset:0];
+    NSLog(@"array -- %@", sequence.array);
+    
+    // 打印日志：
+    /*
+     2018-08-17 17:32:16.880412+0800 TestRACStringSequence[51625:18369711] array -- (
+     a,
+     b,
+     c
+     )
+     */
 }
 
 @end
